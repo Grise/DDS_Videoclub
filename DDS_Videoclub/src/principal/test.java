bc95b2c4c3df95e7a45ee73151eda5c2c85cd3df
Grise <dpguay@gmail.com>
2016-05-07 15:29:06 +0200

    Cambios para conectar con el driver de HSQLDB

--- DDS_Videoclub/src/principal/test.java
+++ DDS_Videoclub/src/principal/test.java
@@ -1,24 +1,17 @@
 package principal;
 
-import java.sql.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class test {
-    public static void main (String[] args){
+
+    public static void main(String[] args) {
         try {
-            String driver = "org.apache.derby.jdbc.EmbeddedDriver";
-            Class.forName(driver).newInstance();
-            String dbURL = "jdbc:derby:data\\dbvideoclub;create=true";
-            Connection conn = DriverManager.getConnection(dbURL);
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("select * from peliculas");
-            while(rs.next()){
-                System.out.println(rs.getString("nombre"));
-            }
-            DriverManager.getConnection("jdbc:derby:;shutdown=true");
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | SQLException ex) {
-            Logger.getLogger(test.class.getName()).log(Level.SEVERE, null, ex);
+            Connection connection = DriverManager.getConnection("jdbc:hsqldb:file:testdb");
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
+
     }
 }
