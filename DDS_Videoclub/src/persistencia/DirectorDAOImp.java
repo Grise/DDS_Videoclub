bf10632c036d6cf993b0d051fe867da12abe6433
Grise <dpguay@gmail.com>
2016-05-07 19:16:28 +0200

    Refactorización para igualar estilos

--- DDS_Videoclub/src/persistencia/DirectorDAOImp.java
+++ DDS_Videoclub/src/persistencia/DirectorDAOImp.java
@@ -1,38 +1,38 @@
-
 package persistencia;
 
 import excepciones.DAOExcepcion;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import persistencia.dto.DirectorDTO;
 
-public class DirectorDAOImp implements IDirectorDAO {
-	
-	protected ConnectionManager connectionManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-	public DirectorDAOImp() throws DAOExcepcion {
-		super();
-		try{
-		connectionManager = new ConnectionManager("dbvideoclub");
-		}
-		catch (ClassNotFoundException e){	
-			throw new DAOExcepcion(e);
-			}
-	}
-	
-	public DirectorDTO buscarDirector(int id) throws DAOExcepcion {
-		try{
-			connectionManager.connect();
-			ResultSet rs=connectionManager.queryDB("select * from DIRECTOR where ID= '"+id+"'");
-			connectionManager.close();
-		
-			if (rs.next())
-				return new DirectorDTO(
-							rs.getInt("ID"),
-							rs.getString("NOMBRE"));
-			else
-				return null;	
-		}
-		catch (SQLException e){	throw new DAOExcepcion(e);}	
-	}
+public class DirectorDAOImp implements IDirectorDAO {
+
+    private ConnectionManager connectionManager;
+
+    public DirectorDAOImp() throws DAOExcepcion {
+        super();
+        try {
+            connectionManager = new ConnectionManager("dbvideoclub");
+        } catch (ClassNotFoundException e) {
+            throw new DAOExcepcion(e);
+        }
+    }
+
+    public DirectorDTO buscarDirector(int id) throws DAOExcepcion {
+        try {
+            connectionManager.connect();
+            ResultSet rs = connectionManager.queryDB("select * from DIRECTOR where ID= '" + id + "'");
+            connectionManager.close();
+
+            if (rs.next())
+                return new DirectorDTO(
+                        rs.getInt("ID"),
+                        rs.getString("NOMBRE"));
+            else
+                return null;
+        } catch (SQLException e) {
+            throw new DAOExcepcion(e);
+        }
+    }
 }
