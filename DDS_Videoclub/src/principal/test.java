dd383014faa6d25a9d240b8464dc77ae2702de40
Grise <dpguay@gmail.com>
2016-05-07 14:21:42 +0200

    Me he cargado la BD en Derby y la he hecho en HSQLDB

--- DDS_Videoclub/src/principal/test.java
+++ DDS_Videoclub/src/principal/test.java
@@ -1,34 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package principal;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author The PinkyCandy
- */
 public class test {
     public static void main (String[] args){
         try {
             String driver = "org.apache.derby.jdbc.EmbeddedDriver";
             Class.forName(driver).newInstance();
-            String dbURL = "jdbc:derby:C:\\Users\\The PinkyCandy\\Documents\\NetBeansProjects\\DDS_Videoclub\\DDS_Videoclub\\data\\dbvideoclub;create=true";
+            String dbURL = "jdbc:derby:data\\dbvideoclub;create=true";
             Connection conn = DriverManager.getConnection(dbURL);
             Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery("select * from peliculas");
             while(rs.next()){
                 System.out.println(rs.getString("nombre"));
             }
+            DriverManager.getConnection("jdbc:derby:;shutdown=true");
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | SQLException ex) {
             Logger.getLogger(test.class.getName()).log(Level.SEVERE, null, ex);
         }
