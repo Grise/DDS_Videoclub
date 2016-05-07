8d594918a3129fed72cafdb8a5ce2bb1e77243c5
Grise <dpguay@gmail.com>
2016-05-07 15:55:22 +0200

    Cambios para conectar con el driver de HSQLDB

--- DDS_Videoclub/src/principal/test.java
+++ DDS_Videoclub/src/principal/test.java
@@ -1,14 +1,18 @@
 package principal;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class test {
 
     public static void main(String[] args) {
         try {
-            Connection connection = DriverManager.getConnection("jdbc:hsqldb:file:testdb");
+            Connection connection = DriverManager.getConnection("jdbc:hsqldb:file:data/bdvc", "sa", "sa");
+            Statement statement = connection.createStatement();
+            String sql = "SELECT * FROM pruebas";
+            ResultSet rs = statement.executeQuery(sql);
+            while (rs.next()) {
+                System.out.println(rs.getString("ID"));
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
