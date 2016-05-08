2885763fd840c9c75aa8ba1bf1eca1dbc68d945a
Grise <dpguay@gmail.com>
2016-05-08 20:17:28 +0200

    Patrón Flyweight

--- DDS_Videoclub/src/logica/PersonaFabrica.java
+++ DDS_Videoclub/src/logica/PersonaFabrica.java
@@ -1,18 +1,40 @@
 package logica;
 
+import java.util.HashMap;
+
 public class PersonaFabrica {
-    public Persona getPersona(String tipoPersona, int id, String nombre){
-        if(tipoPersona == null)
+    private final HashMap<String, Persona> personaMap = new HashMap<>();
+
+    public Persona getPersona(String tipoPersona, int id, String nombre) {
+        if (tipoPersona == null)
             return null;
 
-        if(tipoPersona.equalsIgnoreCase("CLIENTE"))
-            return new Cliente(id, nombre);
+        if (tipoPersona.equalsIgnoreCase("CLIENTE")) {
+            Cliente cliente = (Cliente) personaMap.get(nombre);
+            if (cliente == null) {
+                cliente = new Cliente(nombre);
+                personaMap.put(nombre, cliente);
+            }
+            return cliente;
+        }
 
-        if(tipoPersona.equalsIgnoreCase("EMPLEADO"))
-            return new Empleado(id, nombre);
+        if (tipoPersona.equalsIgnoreCase("EMPLEADO")) {
+            Empleado empleado = (Empleado) personaMap.get(nombre);
+            if (empleado == null){
+                empleado = new Empleado(nombre);
+                personaMap.put(nombre, empleado);
+            }
+            return empleado;
+        }
 
-        if(tipoPersona.equalsIgnoreCase("DIRECTOR"))
-            return new Director(id, nombre);
+        if (tipoPersona.equalsIgnoreCase("DIRECTOR")) {
+            Director director = (Director) personaMap.get(nombre);
+            if (director == null){
+                director = new Director(nombre);
+                personaMap.put(nombre, director);
+            }
+            return director;
+        }
 
         return null;
     }
