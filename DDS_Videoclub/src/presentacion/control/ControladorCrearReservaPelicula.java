93d52a98e5c78b5c01a6ae199c030045177dc0d1
Grise <dpguay@gmail.com>
2016-06-11 17:51:46 +0200

    Comienzo vista controlador.

--- DDS_Videoclub/src/presentacion/control/ControladorCrearReservaPelicula.java
+++ DDS_Videoclub/src/presentacion/control/ControladorCrearReservaPelicula.java
@@ -1,14 +1,39 @@
 package presentacion.control;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.fxml.Initializable;
 
 
 public class ControladorCrearReservaPelicula extends ControladorCasoDeUso {
+    @FXML
+    private TextField inputGenero;
+
+    @FXML
+    private TextField inputDirector;
+
+    @FXML
+    private TextField inputPelicula;
+
+    @FXML
+    private TableView<?> tableViewPeliculas;
+
+    @FXML
+    private Button botonCancelar;
+
+    @FXML
+    private Button botonAceptar;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        stage = new Stage(StageStyle.DECORATED);
+        stage.setTitle("LISTADO DE SUCURSALES");
+        botonCancelar.setOnAction(event -> stage.close());
     }
 }
