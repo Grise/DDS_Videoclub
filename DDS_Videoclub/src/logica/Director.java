1769f887cb450fc022886a1e74dab92c75708010
The PinkyCandy <The PinkyCandy@ThePinkyCandy>
2016-06-11 18:10:52 +0200

    Avances en AlquilerPeliculasLogica

--- DDS_Videoclub/src/logica/Director.java
+++ DDS_Videoclub/src/logica/Director.java
@@ -1,8 +1,11 @@
 package logica;
 
+import java.util.ArrayList;
+
 class Director extends Persona {
     private int id;
     private String nombre;
+    private ArrayList<Pelicula> pelicula;
 
     public Director(int id, String nombre) {
         this.id = id;
@@ -27,6 +30,34 @@ class Director extends Persona {
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+    
+    // Metodos para tratar lista peliculas
+    
+    public void addPelicula(Pelicula p) {
+        pelicula.add(p);
+    }
+
+    public void deletePelicula(Pelicula p) {
+        pelicula.remove(pelicula.indexOf(p));
+    }
+
+    public Pelicula getPelicula(int id) {
+        for (Pelicula p : pelicula) {
+            if (p.getId() == id) {
+                return p;
+            }
+
+        }
+        return null;
+    }
+    
+    public ArrayList<Pelicula> getPelicula() {
+        return pelicula;
+    }
+
+    public void setCoche(ArrayList<Pelicula> coche) {
+        this.pelicula = coche;
     }
 
 }
